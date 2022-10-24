93e8d80eb73b70a7796acbcdc856f791f17e271c
jmohren <johannes.mohren@rwth-aachen.de>
2022-10-24 15:48:02 +0000

    update

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+#https://lcalcagni.medium.com/deploy-your-fastapi-to-aws-ec2-using-nginx-aa8aa0d85ec7 
+#https://dev.to/nick_langat/how-to-deploy-a-fastapi-app-to-aws-ec2-server-46d4 
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Input(BaseModel):
+    name: str
+    age: int
 
 app = FastAPI()
 
@@ -15,4 +23,9 @@ app.add_middleware(
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to the API!"}
+    return {"message": "TestWelcome to the API!"}
+
+@app.post("/test")
+def test(input: Input):
+    da = input.age*2
+    return {"age": da}
