1d9e3e7297af164042ad0672fa7e213a4eecb24c
jmohren <johannes.mohren@rwth-aachen.de>
2022-10-25 16:28:59 +0200

    t

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 server_domain_or_IP="$(curl ipinfo.io/ip)"
 
-cd
-sudo sed -i 's/server_name.*;/server_name $server_domain_or_IP;/' /etc/nginx/sites-enabled/api
+#cd
+#sudo sed -i 's/server_name.*;/server_name $server_domain_or_IP;/' /etc/nginx/sites-enabled/api
 
-cd home/ubuntu/apiv/
 python3 main.py
