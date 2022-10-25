33d2c074a93239a2caef65f2bc52373e93f93932
jmohren <johannes.mohren@rwth-aachen.de>
2022-10-25 14:15:24 +0200

    delete nano

--- start.sh
+++ start.sh
@@ -3,6 +3,4 @@ server_domain_or_IP=curl ipinfo.io/ip
 cd
 sed -i 's/server_name.*;/server_name $server_domain_or_IP;/' /etc/nginx/sites-enabled/api
 
-sudo nano /etc/nginx/sites-enabled/api
-
 python3 main.py
