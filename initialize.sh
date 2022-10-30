2ee44de0ad8260b44dc3d81b81f2aa0575754a35
john8991 <johannes.mohren@rwth-aachen.de>
2022-10-30 16:19:07 +0100

    mv instead of cat

--- initialize.sh
+++ initialize.sh
@@ -9,57 +9,17 @@ virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
 
-#Create gunicorn.socket file
-sudo cat > /etc/systemd/system/gunicorn.socket << ENDOFFILE
-[Unit]
-Description=gunicorn socket
+#Move gunicorn.socket file
+sudo mv files/gunicorn.socket ~/etc/systemd/system
 
-[Socket]
-ListenStream=/run/gunicorn.sock
-
-[Install]
-WantedBy=sockets.target
-ENDOFFILE
-
-#Create gunicorn.service file
-sudo cat > /etc/systemd/system/gunicorn.service << ENDOFFILE
-[Unit]
-Description=gunicorn daemon
-Requires=gunicorn.socket
-After=network.target
-
-[Service]
-User=ubuntu
-Group=www-data
-WorkingDirectory=/home/ubuntu/api
-ExecStart=/home/ubuntu/api/env/bin/gunicorn \
-          --access-logfile - \
-          --workers 5 \
-          --bind unix:/run/gunicorn.sock \
-          --worker-class uvicorn.workers.UvicornWorker \
-          app:app
-
-[Install]
-WantedBy=multi-user.target
-ENDOFFILE
+#Move gunicorn.service file
+sudo mv files/gunicorn.service ~/etc/systemd/system
 
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
 
-#Create api file
-sudo cat > /etc/nginx/sites-enabled/api << ENDOFFILE
-server {
-    listen 80;
-    listen 443 ssl;
-    ssl on;
-    ssl_certificate /etc/nginx/ssl/server.crt;
-    ssl_certificate_key /etc/nginx/ssl/server.key;
-    server_name server_domain_or_IP;
-    location / {
-        proxy_pass http://unix:/run/gunicorn.sock;
-    }
-}
-ENDOFFILE
+#Move api file
+sudo mv files/api ~/etc/nginx/sites-enabled
 
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn
