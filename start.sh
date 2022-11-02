06a13ea8a92af9e419436304dd49f1a93a5d4de8
jmohren <johannes.mohren@rwth-aachen.de>
2022-11-02 15:25:15 +0000

    no restart

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@ server_domain_or_IP="$(curl ipinfo.io/ip)"
 
 sudo sed -i "s/server_name.*;/server_name $server_domain_or_IP;/" /etc/nginx/sites-enabled/api
 
-sudo systemctl daemon-reload
-sudo systemctl restart gunicorn
-sudo systemctl restart nginx
 sudo service nginx restart
 
 source env/bin/activate
